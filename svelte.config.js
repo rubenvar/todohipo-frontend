0eee0af704576eaadafdc52d0c5eaa6c5311dcb5
Rubén <hey@rubenvara.io>
2021-12-12 15:46:45 +0100

    netlify

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,13 @@
+/* eslint-disable import/no-extraneous-dependencies */
 /** @type {import('@sveltejs/kit').Config} */
-// eslint-disable-next-line import/no-extraneous-dependencies
 import preprocess from 'svelte-preprocess';
+import netlify from '@sveltejs/adapter-netlify';
 
 const config = {
   kit: {
     // hydrate the <div id="svelte"> element in src/app.html
     target: '#svelte',
+    adapter: netlify(),
   },
   preprocess: preprocess(),
 };
